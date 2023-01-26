aa5ee07b4a594e74571c80cd48d9d4a73dc8cd63
Genifer Abalos <genifer.abalos@gmail.com>
2023-01-26 22:24:07 +0800

    experiment on other parameters: n (number of responses), stop (seperator of questions)

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+# openai.api_key_path = r".env"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
@@ -11,11 +12,14 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     if request.method == "POST":
         animal = request.form["animal"]
+        print(generate_prompt(animal))
         response = openai.Completion.create(
             model="text-davinci-002",
+            # engine="text-davinci-002",
             prompt=generate_prompt(animal),
             temperature=0.6,
         )
+        # return redirect(url_for("index", result=response.choices[0].text))
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
